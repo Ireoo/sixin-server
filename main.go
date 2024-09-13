d2fd04a4539157d18d83a362256d71b72ecd466c
Jack Wang <jack.wang@integem.com>
2024-09-14 02:22:20 +0800

    更新程序

--- main.go
+++ main.go
@@ -16,6 +16,13 @@ import (
 )
 
 func main() {
+	// 定义命令行参数
+	dbTypeFlag := flag.String("db-type", "sqlite", "数据库类型 (postgres, mongodb, sqlite)")
+	dbConnFlag := flag.String("db-uri", "./database.db", "数据库连接地址")
+	hostFlag := flag.String("host", "0.0.0.0", "服务器主机名")
+	portFlag := flag.Int("port", 8000, "服务器端口")
+	flag.Parse()
+
 	// 创建必要的文件夹
 	folders := []string{"data", "image", "avatar", "audio", "video", "attachment", "emoticon", "url", "database"}
 	for _, folder := range folders {
@@ -25,9 +32,21 @@ func main() {
 		}
 	}
 
-	// 初始化数据库
-	if err := database.InitSqliteDB(); err != nil {
-		log.Fatal("Failed to initialize database:", err)
+	// 优先使用命令行参数，其次是环境变量，最后是默认值
+	dbType := database.DatabaseType(*dbTypeFlag)
+	if dbType == "" {
+		dbTypeStr := os.Getenv("DB_TYPE")
+		if dbTypeStr == "" {
+			dbTypeStr = "sqlite" // 默认数据库类型
+		}
+	}
+
+	dbConn := *dbConnFlag
+	if dbConn == "" {
+		dbConn = os.Getenv("DB_URI")
+		if dbConn == "" {
+			dbConn = "./database.db" // 默认数据库连接地址
+		}
 	}
 
 	// 创建Socket.IO服务器
@@ -38,24 +57,18 @@ func main() {
 		// 创建 base.Base 实例
 		baseInstance := &base.Base{}
 
-		// 用户可以选择数据库类型
-		dbType := database.SQLite // 或 database.MongoDB
-
-		err := database.InitDatabase(dbType, "")
+		// 初始化数据库
+		err := database.InitDatabase(dbType, dbConn)
 		if err != nil {
 			log.Fatalf("初始化数据库失败: %v", err)
 		}
-		// 获取数据库连接
-		db := database.GetSqliteDB()
+
+		// 获取当前数据库连接
+		db := database.GetCurrentDB()
 		// 设置Socket.IO事件处理
-		socket.SetupSocketHandlers(server, db.DB, baseInstance)
+		socket.SetupSocketHandlers(server, db.GetDB(), baseInstance)
 		log.Println("Socket.IO服务器创建成功")
 	}
-
-	// 定义命令行参数
-	hostFlag := flag.String("host", "", "服务器主机名")
-	portFlag := flag.Int("port", 0, "服务器端口")
-	flag.Parse()
 
 	// 优先使用命令行参数，其次是环境变量，最后是默认值
 	host := *hostFlag
